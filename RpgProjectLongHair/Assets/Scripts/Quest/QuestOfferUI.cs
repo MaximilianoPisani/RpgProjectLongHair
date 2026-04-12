b16f6d7d6a190727fb1480a0f44abec244e61167
Fabián Enrique García <garciafabianenrique79@gmail.com>
2026-04-12 11:53:09 -0300

    UI Quest trigger
    + Prefab de UI base para tomar la misíon con NPC

---
+++ RpgProjectLongHair/Assets/Scripts/Quest/QuestOfferUI.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestOfferUI : MonoBehaviour
+{
+    [Header("Panel")]
+    [SerializeField] private GameObject _panel;
+
+    [Header("Texts")]
+    [SerializeField] private TextMeshProUGUI _txtQuestName;
+    [SerializeField] private TextMeshProUGUI _txtQuestDescription;
+
+    [Header("Buttons")]
+    [SerializeField] private Button _btnAccept;
+    [SerializeField] private Button _btnCancel;
+
+    private QuestController _questController;
+    private QuestDataSO _questData;
+
+    private void Awake()
+    {
+        _btnAccept.onClick.AddListener(OnAccept);
+        _btnCancel.onClick.AddListener(OnCancel);
+        _panel.SetActive(false);
+    }
+
+    public void Show(QuestDataSO data, QuestController controller)
+    {
+        _questData = data;
+        _questController = controller;
+        _txtQuestName.text = data.questName;
+        _txtQuestDescription.text = data.questDescription;
+        _panel.SetActive(true);
+
+        // Mostrar cursor y bloquear input de camara
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void OnAccept()
+    {
+        _questController.RPC_StartMission(_questData.questId, default);
+        _panel.SetActive(false);
+
+        // Ocultar cursor y devolver el control
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnCancel()
+    {
+        _panel.SetActive(false);
+    }
+}
--- RpgProjectLongHair/Assets/Scripts/Quest/QuestTrigger.cs
+++ RpgProjectLongHair/Assets/Scripts/Quest/QuestTrigger.cs
@@ -1,8 +1,11 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class QuestTrigger : MonoBehaviour
 {
     [SerializeField] private string _missionId = QuestIds.QUEST_TEST;
+    [SerializeField]private QuestOfferUI _questOfferUI;
+
     private bool _isPlayerInZone;
     private QuestController _questController;
 
@@ -29,7 +32,16 @@ public class QuestTrigger : MonoBehaviour
         if (_questController == null) return;      // no hay QuestController -> ignorar
         if (_questController.CurrentQuest != null) return; // ya tiene misión -> ignorar
 
-        // Si pasó todos los filtros -> iniciar mision
-        _questController.RPC_StartMission(_missionId, default);
+        // Si pasó todos los filtros -> iniciar mision , inicio directo sin UI
+        //_questController.RPC_StartMission(_missionId, default);
+
+        // Inicio de quest con UI
+        var questData = Resources.Load<QuestDataSO>($"Quest/{_missionId}");
+
+        if (questData != null)
+        {
+            _questOfferUI.Show(questData, _questController);
+        }
+
     }
 }
