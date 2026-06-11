1b12ee13b52894631619efad68734f06aa270f36
BraSchoepf <Deidadmanca2@gmail.com>
2026-06-11 14:02:46 -0300

    new statues_for mission

---
+++ RpgProjectLongHair/Assets/Scripts/Environment/Statues/StatuePose.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class StatuePose : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private AnimationClip clip;
+    [SerializeField] private int frame;
+
+    private void OnValidate()
+    {
+        ApplyPose();
+    }
+
+    private void Start()
+    {
+        ApplyPose();
+    }
+
+    private void ApplyPose()
+    {
+        if (animator == null || clip == null)
+            return;
+
+        float normalizedTime = frame / (clip.length * clip.frameRate);
+
+        animator.Play(0, 0, normalizedTime);
+        animator.Update(0f);
+        animator.speed = 0f;
+    }
+}
